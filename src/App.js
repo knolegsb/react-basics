78c135b2efb70bc4550acd913e438cd7f3953e3f
seancbs <sean.y@centurybizsolutions.com>
2020-09-12 17:36:22 -0700

    made a counter

--- src/App.js
+++ src/App.js
@@ -2,25 +2,37 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <h1>Hello World</h1>
+//       </header>
+//     </div>
+//   );
+// }
+
+class App extends React.Component {
+  state = {
+    counter: 0
+  }
+
+  increment = (num) => {
+    this.setState({
+      counter: this.state.counter + num
+    });
+  }
+
+  render() {
+    return (
+      <div className="container">
+        <h1>Counter Application: {this.state.counter}</h1>
+        <button className="btn btn-primary" onClick={() => this.increment(1)}>Increment</button>
+        <button className="btn btn-warning" onClick={() => this.increment(-1)}>Decrement</button>
+      </div>
+    )
+  }
+
 }
 
 export default App;
