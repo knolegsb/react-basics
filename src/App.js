b313a4fbda3765cafde3d4f592c39686e04ab3c9
seancbs <sean.y@centurybizsolutions.com>
2020-09-12 17:59:58 -0700

    used context

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
+//import logo from './logo.svg';
 import './App.css';
 
 // function App() {
@@ -12,27 +12,41 @@ import './App.css';
 //   );
 // }
 
-class App extends React.Component {
-  state = {
-    counter: 0
-  }
+// class App extends React.Component {
+  // state = {
+  //   counter: 0
+  // }
 
-  increment = (num) => {
-    this.setState({
-      counter: this.state.counter + num
-    });
-  }
+  // increment = (num) => {
+  //   this.setState({
+  //     counter: this.state.counter + num
+  //   });
+  // }
 
-  render() {
+
+
+  // render() {
+
+  const App = () => {
+
+    //const [counter, setCounter] = useState(0);
+    const counterArr = useState(0);
+
+    const increment = (num) => {
+      //setCounter(counter + num)
+      counterArr[1](counterArr[0] + num);
+    }
+
     return (
       <div className="container">
-        <h1>Counter Application: {this.state.counter}</h1>
-        <button className="btn btn-primary" onClick={() => this.increment(1)}>Increment</button>
-        <button className="btn btn-warning" onClick={() => this.increment(-1)}>Decrement</button>
+        {/* <h1>Counter Application: {counter}</h1> */}
+        <h1>Counter Application: {counterArr[0]}</h1>
+        <button className="btn btn-primary" onClick={() => increment(1)}>Increment</button>
+        <button className="btn btn-warning" onClick={() => increment(-1)}>Decrement</button>
       </div>
     )
   }
 
-}
+//}
 
 export default App;
